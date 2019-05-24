206a5198efa89343c9731cb67394e16e84f4b48b
Sebastian Potocnjak <sebastian.potocnjak@usach.cl>
2019-05-24 15:47:09 -0400

    probando git

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,7 +10,8 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
-     */
+   
+  */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
