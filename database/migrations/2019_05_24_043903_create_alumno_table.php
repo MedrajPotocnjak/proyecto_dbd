7abff23b741d49950648c769984c8f86adf2aab9
Gerardo <gerardo.ternero@usach.cl>
2019-05-28 12:49:51 -0300

    Creacion de atributos alumno

--- database/migrations/2019_05_24_043903_create_alumno_table.php
+++ database/migrations/2019_05_24_043903_create_alumno_table.php
@@ -14,8 +14,28 @@ class CreateAlumnoTable extends Migration
     public function up()
     {
         Schema::create('alumno', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrement('id');
+            $table->integer('rut')->unique();
             $table->timestamps();
+            $table->string("nombre", 30);
+            $table->string("apellido_paterrno", 15);
+            $table->string("apellido_materrno", 15);
+            $table->integer("nivel");
+            $table->string("ingreso",5); /* 1/2019 */ 
+            $table->string("password",14);
+            $table->integer("asignaturas_aprovadas");
+            $table->float("nas");
+            $table->float("ppa");
+            $table->float("nar");
+            $table->timestamps("fecha_nacimiento");
+            $table->string("nacionalidad",20);
+            $table->string("nacionalidad",12);
+            $table->char("sexo");
+            $table->string("telefono",12);
+            $table->string("region",30);
+            $table->string("provincia",30);
+            $table->string("comuna",30);
+            $table->string("correo",40);
         });
     }
 
