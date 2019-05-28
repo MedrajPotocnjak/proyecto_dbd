3051412784cd9808ee927868c4ae93715676f746
Javiera A <javiera.ayala@usach.cl>
2019-05-28 11:50:09 -0400

    Creacion atributos matricula

--- database/migrations/2019_05_24_045040_create_matricula_table.php
+++ database/migrations/2019_05_24_045040_create_matricula_table.php
@@ -14,8 +14,12 @@ class CreateMatriculaTable extends Migration
     public function up()
     {
         Schema::create('matricula', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->bigIncrements('codigo_matricula');
+            /* Pendiente: Considerar que la llave foranea es el rut del alumno */
+            $table->foreing('rut')->references('rut')->on-('alumno')->onDelete('cascade');
+             $table->string('estado_matricula',20);
+             /*Como integer ya que money es raro D:*/
+            $table->integer('costo');
         });
     }
 
