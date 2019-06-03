098d31bc90ec29a0d816f9a516586b2c9b6d7c89
Ricardo Ruz <ricardo.ruz@usach.cl>
2019-06-03 13:33:31 -0400

    Cambios en Modelos
    Cambios en Modelos Mensaje y Matricula.

--- app/matricula.php
+++ app/matricula.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class matricula extends Model
 {
     //
+    protected $table='matriculas';
+    protected $fillable=['estado_matricula','costo'];
+    public function pagos(){
+    	return $this->belogsTo('App\Pago');
+    }
 }
