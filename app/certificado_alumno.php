606b110a27479509882fe818b5a35b1e3e798dce
Sebastian Potocnjak <sebastian.potocnjak@usach.cl>
2019-06-03 10:07:24 -0400

    Cambios en Modelos
    Modelos: Certificado_Alumno, Seccion_Sala

--- app/certificado_alumno.php
+++ app/certificado_alumno.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class certificado_alumno extends Model
 {
     //
+    protected $table = 'certificados_alumnos';
+    public function certificados(){
+    	return $this->belongsTo('App\Certificado');
+    }
+    public function alumnos(){
+    	return $this->belongsTo('App\Alumno');
+    }
 }
