5726a2f20e1466c2d67b01a1ea3b0dedc73e1b5d
Ricardo Ruz <ricardo.ruz@usach.cl>
2019-06-03 01:25:09 -0400

    Cambios en Modelo
    Cambios en Modelos Carrera, Coordinador_Docente y Departamento

--- app/coordinador_docente.php
+++ app/coordinador_docente.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class coordinador_docente extends Model
 {
     //
+    protected $table= 'coordinador_docente';
+    protected $fillable=['password','nombres','apellido_paterno','apellido_materno'];
+    publci function departamento(){
+    	return $this->belongsTo('App\Departamento');
+    }
 }
--- app/departamento.php
+++ app/departamento.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class departamento extends Model
 {
     //
+    protected $table= 'departamentos';
+    protected $fillable='nombre';
+    public function coordinador_docente(){
+    	return $this->hasOne('App\Coordinador_Docente');
+    }
+    public function estudios(){
+    	return $this->hasMany('App\Estudio');
+    }
+    public function carreras(){
+    	return $this->hasMany('App\Carrera');
+    }
 }
