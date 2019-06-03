94ce6ba749beeb7f0431391396894b8aaa31bd98
Ricardo Ruz <ricardo.ruz@usach.cl>
2019-06-03 02:14:30 -0400

    Cambio en Modelo
    Cambio en Modelo Certificado

--- app/certificado.php
+++ app/certificado.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class certificado extends Model
 {
     //
+    protected $table='certificados';
+    protected $fillable=['codigo_verificacion','contenido','fecha','ruta_formato'];
+    public function certificados_alumno(){
+    	return $this->hasMany('App\Certificado_Alumno');
+    }
 }
