734102fa5de05442693f94c6e6f443aee1fd5e23
Gerardo <gerardo.ternero@usach.cl>
2019-06-21 18:15:15 -0300

    Trigger SQL

--- database/script/triggers.sql
+++ database/script/triggers.sql
@@ -1,21 +1,24 @@
 /* Trigger para el calculo del promedio cuando las notas han sido ingresadas */
 CREATE OR REPLACE FUNCTION calcular_promedio()
 RETURNS trigger AS
-$BODY$
+$calcular_promedio$
+DECLARE
+prom_control float;
+prom_notas float;
+
 BEGIN
-  	AVG(seccion_alumno.nota_p1, seccion_alumno.nota_p1nota_p2, seccion_alumno.nota_p1nota_p3, AVG(seccion_alumno.nota_p1nota_c1, seccion_alumno.nota_p1nota_c2, seccion_alumno.nota_p1nota_c3)) AS promedito
-	UPDATE seccion_alumno
-    SET promedio = promedito
-    WHERE seccion_alumno.rut_alumno = NEW.rut_alumno;
-    RETURN NEW;
+	SELECT AVG(
+  	UPDATE seccion_alumno
+    SET promedio = (nota_p1 + nota_p2 + nota_p3 + (nota_c1 + nota_c2 + nota_c3) ) / 4
+    RETURN NULL;
 END;
-$BODY$
+$calcular_promedio$
 LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS promedio ON seccion_alumno;
 
 CREATE TRIGGER promedio
-BEFORE INSERT ON seccion_alumno
+AFTER UPDATE ON seccion_alumno
 EXECUTE PROCEDURE calcular_promedio();
 
 /*    */
