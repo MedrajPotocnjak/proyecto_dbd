2d8aec7be5db5b26d17b266b88742a9b7f22a821
Sebastian Potocnjak <sebastian.potocnjak@usach.cl>
2019-07-26 01:27:55 -0400

    Corrección Documentos Profesor

--- resources/js/components/profesor/DocumentosProfesor.vue
+++ resources/js/components/profesor/DocumentosProfesor.vue
@@ -1,62 +1,142 @@
 <template>
     <div>
-      <v-toolbar flat color = "white">
-        <v-toolbar-title> Documentos </v-toolbar-title>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>Mis Documentos</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
         <v-spacer></v-spacer>
       </v-toolbar>
       <v-data-table
         :headers="headers"
         :items="documentos"
-        :expand="expand"
-        item-key="tipo"
+        class="elevation-1"
       >
         <template v-slot:items="props">
-          <tr @click="props.expanded = !props.expanded">
-            <td>{{ props.item.tipo }}</td>
-          </tr>
+          <td>{{ props.item.name }}</td>
+          <td class="text-center">{{ props.item.fecha }}</td>
+          <td class="text-xs-right"> <v-btn small color = "success"> Descargar </v-btn> </td>
+          <td class="justify-center">
+            <v-icon
+              small
+              @click="deleteItem(props.item)"
+            >
+              delete
+            </v-icon>
+          </td>
         </template>
-        <template v-slot:expand="props">
-          <div v-for = "i in documentos">
-            <div v-for = "j in i.documento"> <p class="text-sm-left"> {{j}} </p>
-              <v-card flat >
-                <v-card-text> 
-                  <v-card-actions>
-                    <v-btn small color = "success"> Descargar </v-btn>
-                    <v-btn small color = "primary"> Subir <v-icon right dark> cloud_upload </v-icon> </v-btn>
-                  </v-card-actions>  
-                </v-card-text> 
-              </v-card>
-            </div>  
-          </div>   
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
         </template>
       </v-data-table>
+      <v-card>
+        <v-card-text> 
+          <v-card-actions>
+            <v-btn small color = "primary"> Subir <v-icon right dark> cloud_upload </v-icon> </v-btn>
+          </v-card-actions>  
+        </v-card-text> 
+      </v-card>
     </div>
 </template>
 
 <script>
   export default {
     name: "DocumentosProfesor",
-    data () {
-      return {
-        expand: false,
-        headers: [
-          {
-            text: 'Documentos',
-            align: 'left',
-            value: 'tipo',
-          },
-        ],
-        documentos: [
-          {
-            tipo: 'Material',
-            documento: [
-              'Investigación',
-              'Clases',
-              'Guias',
-            ]
-          },   
-        ],
-      }
+    data: () => ({
+    dialog: false,
+    headers: [
+      {
+        text: 'Documentos',
+        align: 'left',
+        sortable: false,
+        value: 'name'
+      },
+      { text: 'Fecha', value: 'fecha' },
+      {},
+      { text: 'Actions', value: 'name', sortable: false }
+    ],
+  }),
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
+
+  created () {
+    this.initialize()
+  },
+
+  methods: {
+    initialize () {
+      this.documentos = [
+        {
+          name: 'Estudio de los procesos.pdf',
+          fecha: "26/07/19",
+          url: " ",
+        },
+        {
+          name: 'Trabajo de la fisica cuantica.pdf',
+          fecha: "10/07/19",
+          url: " ",
+        },
+        {
+          name: 'Paper de estudio.pdf',
+          fecha: "05/04/19",
+          url: " ",
+        },
+        {
+          name: 'Investigacíon.pdf',
+          fecha: "20/03/19",
+          url: " ",
+        },
+        {
+          name: 'Tesis doctoral.pdf',
+          fecha: "01/02/19",
+          url: " ",
+        },
+        {
+          name: 'Presentación.pdf',
+          fecha: "20/03/19",
+          url: " ",
+        },
+        {
+          name: 'Documento.pdf',
+          fecha: "18/06/19",
+          url: " ",
+        },
+        {
+          name: 'Investigacíon2.pdf',
+          fecha: "12/01/19",
+          url: " ",
+        },
+        {
+          name: 'Fechas.pdf',
+          fecha: "25/05/19",
+          url: " ",
+        },
+        {
+          name: 'Laboratorio.pdf',
+          fecha: "28/02/19",
+          url: " ",
+        }
+      ]
+    },
+
+    deleteItem (item) {
+      const index = this.documentos.indexOf(item)
+      confirm('Seguro que deseas eliminar este elemento?') && this.documentos.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+        setTimeout(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        }, 300)
+      },
     }
   }
 </script>
