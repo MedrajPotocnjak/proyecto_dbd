9debce40b8c00c87c5f6ce35515dc6b0d98037c6
Javiera A <javiera.ayala@usach.cl>
2019-07-26 08:04:52 -0400

    bladesSimple

---
+++ resources/views/certificadoalumno.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title> Certificado TipoCertificado</title>
+
+</head>
+
+<body>
+   
+   Este Certificado acredita a: NombreAlumno, 
+   Cedula de identidad nacional Numero RutAlumno,
+   De la carrera CarreraAlumno
+   
+   Para los tramites de: TipoCertificado
+
+
+   Fecha Emision: Fecha
+   Valido por 30 dias desde el momento de emision.
+
+
+</body>
+
+</html>
